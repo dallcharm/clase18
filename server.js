f088222e45bc38693f28326300318b73d2b82698
dallcharm <dallcharm@gmail.com>
2019-06-14 19:23:53 -0500

    add public folder

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
 
-app.get('/', (request,reponse) => {
-    reponse.send('Hello World')
-})
+//app.get('/', (request,reponse) => {
+//    reponse.send('Hello World')
+//})
 
 app.listen(9000)
